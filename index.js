22457d1f82f7cf929afb6988893ba14bbc37c514
zerry101 <macone394@gmail.com>
2023-07-04 23:57:40 +0530

    m

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 import * as features from "./features.js";
-import  http from "http";
+import http from "http";
+import fs   from "fs";
 // const name=require("./features");
 // const http = require("http");
 
 console.log(http);
-
-
+const home = fs.readFileSync("./index.html");
 const server = http.createServer((req, res) => {
-    
+
     if (req.url === "/about") {
-        res.end(`<h1>${features.randNumber()}</h1>`);
-    }
-    else if (req.url === "/home") { 
+        // res.end(`<h1>${features.randNumber()}</h1>`);
+
+        res.end(home);
+
+    }   
+    else if (req.url === "/home") {
         res.end("<h1>Home Page</h1>");
+
     }
     else {
         res.end("<h1>Page Not Found </h1>");
     }
-    
+
     console.log(features.name);
     console.log(features.name1);
     console.log(features.name2);
