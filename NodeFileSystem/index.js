d7b6fcb419f943819cbd6eb4ee3e54a2610311cf
zerry101 <macone394@gmail.com>
2023-07-11 21:53:16 +0530

    m

---
+++ NodeFileSystem/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const { readFile } = require('fs/promises');
+const path = require('path');
+const { dirname } = require('path/posix');
+const dirpath = __dirname;
+console.log(path.join(__dirname, "files"));
+console.log(__dirname);
+
+for (let i = 0; i < 2; i++) {
+    fs.writeFileSync(`${dirpath}/apple${i}.txt`, `a simple textfile ${i}`)
+}
+
+fs.readdir(dirpath, (err, files) => {
+    console.log(files);
+    files.forEach((item) => {
+        console.log(item);
+      readFile(`${dirpath}/${item}`).then((data)=>{
+        console.log(data);
+      });  
+    })
+})
