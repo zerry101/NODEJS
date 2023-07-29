2b477050e03462f4cc36e3052cf2facfb9141521
Zishan Shaikh <macone394@gmail.com>
2023-07-29 09:22:32 +0530

    m

--- NodeExpress/index.js
+++ NodeExpress/index.js
@@ -1,7 +1,14 @@
 const express=require("express");
 const app=express();
 app.get('/',(req,res)=>{
+    console.log("data send by browser",req.query.name);
+    res.send("Hello ,this is "+req.query.name);
+})
+app.get('/home',(req,res)=>{
     res.send("Hello ,this is home");
+})
+app.get('/about',(req,res)=>{
+    res.send("Hello ,this is about");
 })
 
 app.listen(3000,()=>{
