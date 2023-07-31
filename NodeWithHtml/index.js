1835083cd6944033b04a5eb112080066a1ed18ba
Zishan Shaikh <macone394@gmail.com>
2023-07-31 20:16:49 +0530

    m

--- NodeWithHtml/index.js
+++ NodeWithHtml/index.js
@@ -7,3 +7,18 @@ const publicPath=path.join(__dirname,'public');
 app.use(express.static(publicPath));
 
 app.listen(5000);
+
+app.get('',(_,res)=>{
+    res.sendFile(`${publicPath}/home.html`)
+})
+app.get('/about',(_,res)=>{
+    res.send(__dirname);
+    res.sendFile(`${publicPath}/about.html`)
+})
+
+app.get('*',(_,res)=>{
+    res.sendFile(`${publicPath}/nopage.html`);
+})
+
+
+
