5978d85411719fe1c8ca1e78cde693f518335d2c
Zishan Shaikh <macone394@gmail.com>
2023-08-15 20:09:43 +0530

    added inoice generator backend

---
+++ InvoiceGeneratorBackend/customerInfoSchemaModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+// const mongoose=require('mongoose');
+
+const customerDataSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    contactno: { type: Number, required: true },
+    GSTNO: { type: String, required: true },
+    transportationmode: { type: String, required: true },
+    vehiclenumber: { type: String, required: true },
+    dateofsupply: { type: Object, required: true },
+    placeofsupply: { type: String, required: true },
+    shippedto: { type: String, required: true },
+    productData: { type: String, required: true },
+    grandtotal: { type: Number, required: true },
+  },
+  { collection: "customerData" }
+);
+
+module.exports = mongoose.model("customerData", customerDataSchema);
--- InvoiceGeneratorBackend/index.js
+++ InvoiceGeneratorBackend/index.js
@@ -1,23 +1,30 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/NewDb");
+require("./config");
+const customerInfo = require("./customerInfoSchemaModel");
 
-// const mongoose=require('mongoose');
+const express = require("express");
 
-const customerDataSchema = new mongoose.Schema(
-  {
-    name: String,
-    address: String,
-    contactno: Number,
-    GSTNO: String,
-    transportationmode: String,
-    vehiclenumber: String,
-    dateofsupply: Object,
-    placeofsupply: String,
-    shippedto: String,
-    productData: String,
-    grandtotal: Number,
-  },
-  { collection: "customerData" }
-);
+const app = express();
 
-module.exports = mongoose.model("customerData", customerDataSchema);
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("lisening on port 3000");
+});
+
+app.post("/create", async (req, res) => {
+  console.log(req.body);
+  const data = new customerInfo(req.body);
+
+  try {
+    await data.validate();
+    const result = await data.save();
+    res.status(201).json(result); // Respond with 201 Created status and saved data
+  } catch (error) {
+    if (error.name == "ValidationError") {
+      res.status(400).send(error.message);
+    } else {
+      console.error("Server Erro:r", error);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+});
